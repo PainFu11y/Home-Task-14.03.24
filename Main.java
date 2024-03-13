8e7e1de6b788f74cfcb10c3d1e0e3ccce0e2d1d0
PainFu11y <162672019+PainFu11y@users.noreply.github.com>
2024-03-13 17:58:37 +0400

    Update and rename README.md to Main.java

---
+++ Main.java
@@ -0,0 +1,71 @@
+public class Main {
+    public static void main(String[] args) {
+//        int count = 1;
+//        for (int i = 0; i < 17; i++) {
+//            for (int j = 0; j < count; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//            count++;
+//        }
+
+//        ----------------------------
+//        int count = 8;
+//        for (int i = count; i > 0; i--) {
+//            for (int j = count; j > 0; j--) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//            count--;
+//        }
+
+
+        //Homework 2 task 1
+//        int [] arr = new int[10];
+//        for (int i = 0; i < arr.length-1; i++) arr[i] = 2;
+//        for (int i = 0; i < arr.length-1; i++) System.out.print(arr[i]+" ");
+
+        //Homework 2 task 2
+//        int [] arr = new int[1000];
+//        for (int i = 0; i < arr.length-1; i++) arr[i]= i+1;
+//        for (int i = 0; i < arr.length-1; i++) System.out.println(arr[i]);
+
+        //Homework 2 task 3  (-20:20) kent
+//        int [] arr = new int[21];
+//        int count = -19;
+//        for (int i = 0; i < 21; i++) {
+//            arr[i] = count;
+//            count +=2;
+//        }
+//   for (int i = 0; i < arr.length-1; i++) System.out.println(arr[i]);
+
+        // Homework 3 Task 4
+//        int [] arr = {2,4,5,6,8,25,125,80};
+//        for(int tiv: arr ){
+//            if(tiv % 5 == 0 ){
+//                System.out.println(tiv);
+//            }
+//        }
+        //Homework 3 task 5
+//        int [] arr = {1,1234,35,123,5426,123,1234,62,32};
+//        for (int i = 0; i < arr.length-1; i++) {
+//            if ( arr[i] > 24.12 && arr[i] < 476.23 ) System.out.println(arr[i]);
+//        }
+
+        //Homework 3 task 6
+//        int [] arr = {1,3,56,4345,-32,4,13,-1324,5};
+//        for (int i = 0; i < arr.length-1; i++) {
+//            if(arr[i]%2==0) System.out.println(arr[i]);
+//        }
+
+        //Homework 4 task 7
+//        int x = 20; //0<x<21
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println(x + " x " + i + " = " + x*i);
+//        }
+
+
+
+
+    }
+}
